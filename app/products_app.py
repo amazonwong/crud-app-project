e344b150705ec0a5312f0b5a6e182f351a37ddd5
amazonwong <ttw257@stern.nyu.edu>
2017-07-21 19:55:20 -0400

    Print listing products

--- app/products_app.py
+++ app/products_app.py
@@ -1,7 +1,7 @@
 # CRUD APP PROJECT
 
 # READ PRODUCTS CSV
-
+import operator
 import csv
 
 products = []
@@ -30,39 +30,23 @@ menu = """There are {0} products in the database. Please select an operation:
 
 """.format(len(products))
 
-
-
-
-#print("THERE ARE " + str(len(products)) + " PRODUCTS:")
-
-#products = sorted(products, key=operator.itemgetter('name'))
-
-#for product in products:
-    #price_usd = ' (${0:.2f})'.format(product["price"])
-    #print(" + " + product["name"] + price_usd)
-
-
-
-
-# example of manipulating/changing the products list
-#example_new_product = {"id": 100, "name": "New Item", "aisle": "snacks", "department": "snacks", "price":1.99}
-#products.append(example_new_product)
-
-
-#other_path = "data/other_products.csv"
-#with open(other_path, "w") as csv_file:
-    #writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
-    #writer.writeheader() # uses fieldnames set above
-    #for product in products:
-        #writer.writerow(product)
-
-
-
 chosen_operation = input(menu)
 chosen_operation = chosen_operation.title()
 
+products = []
+
+csv_file_path = "data/products.csv"
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        products.append(row)
+
 def list_product():
-    print("LISTING PRODUCTS")
+    print("""THERE ARE " + str(len(products)) "PRODUCTS:""")
+
+    for product in products:
+        print(product)
 
 def show_product():
     print("SHOWING PRODUCTS")
@@ -98,6 +82,34 @@ elif chosen_operation == "Create": create_product()
 elif chosen_operation == "Update": update_product
 elif chosen_operation == "Destroy": destroy_product
 else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+
+
+#print("THERE ARE " + str(len(products)) + " PRODUCTS:")
+
+#products = sorted(products, key=operator.itemgetter('name'))
+
+#for product in products:
+    #price_usd = ' (${0:.2f})'.format(product["price"])
+    #print(" + " + product["name"] + price_usd)
+
+
+
+
+# example of manipulating/changing the products list
+#example_new_product = {"id": 100, "name": "New Item", "aisle": "snacks", "department": "snacks", "price":1.99}
+#products.append(example_new_product)
+
+
+#other_path = "data/other_products.csv"
+#with open(other_path, "w") as csv_file:
+    #writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    #writer.writeheader() # uses fieldnames set above
+    #for product in products:
+        #writer.writerow(product)
+
+
+
+
 
 
 
